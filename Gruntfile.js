8b1010a66baa5e27fc95281d284681131dc10758
Matthew Daly <matthewbdaly@gmail.com>
2015-07-19 18:45:03 +0100

    Can now compile React templates for use on client side

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,17 @@ module.exports = function (grunt) {
             all: [
                 'index.js'
             ]
+        },
+        reactify: {
+          'public/jsx': 'components/*.jsx'
         }
     });
 
     // Load tasks
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-reactify');
 
     // Register tasks
     grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('default', ['reactify']);
 };
