8f490d296252d191f33dca247dff9a0c40aefb1e
Matthew Daly <matthewbdaly@gmail.com>
2015-07-19 19:00:10 +0100

    Added additional components

--- components/html.jsx
+++ components/html.jsx
@@ -6,10 +6,16 @@ var Html = React.createClass({
       <html>
         <head>
           <title>Isomorphic JavaScript for the win</title>
+          <link rel="stylesheet" type="text/css" href="/bower_components/sass-bootstrap/dist/css/bootstrap.min.css" />
+          <link rel="stylesheet" type="text/css" href="/bower_components/sass-bootstrap/dist/css/bootstrap-theme.min.css" />
         </head>
         <body>
             <div id='view' dangerouslySetInnerHTML={{__html: this.props.body}} />
         </body>
+        <script language="javascript" type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
+        <script language="javascript" type="text/javascript" src="/bower_components/sass-bootstrap/dist/js/bootstrap.min.js"></script>
+        <script language="javascript" type="text/javascript" src="/bower_components/react/react.min.js"></script>
+        <script language="javascript" type="text/javascript" src="/jsx/index.js"></script>
       </html>
     );
   }
